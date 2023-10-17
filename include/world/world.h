8b9468553154c18a1fb70d8b90cd5ddf3921ffd1
Alexander Orlov <me@0xceed.com>
2023-10-17 12:18:31 +0300

    Basic 'tick' method on the world.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- include/world/world.h
+++ include/world/world.h
@@ -10,5 +10,6 @@ typedef struct yc_env_world {
 } yc_env_world_t;
 
 yc_env_world_status_t yc_env_world_init(yc_res_map_t *source, yc_env_world_t **destination);
+yc_env_world_status_t yc_env_world_tick(yc_env_world_t *world);
 
 #endif //LIBYCENV_INCLUDE_WORLD_H
--- source/world.c
+++ source/world.c
@@ -14,3 +14,13 @@ yc_env_world_status_t yc_env_world_init(yc_res_map_t *source, yc_env_world_t **d
 
     return YC_ENV_WORLD_STATUS_OK;
 }
+
+yc_env_world_status_t yc_env_world_tick(yc_env_world_t *world) {
+    if (NULL == world) { return YC_ENV_WORLD_STATUS_MEM; }
+    if (NULL == world->map) { return YC_ENV_WORLD_STATUS_MEM; }
+
+    // TODO: Advance the world's state to next tick.
+    world->map->ticks += 1;
+
+    return YC_ENV_WORLD_STATUS_OK;
+}
