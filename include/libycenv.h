86ee01bbac312e12a45871e3a8f679da5899567e
Alexander Orlov <me@0xceed.com>
2023-10-25 02:22:36 +0300

    Updated structs' layout to conventions between repos.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- include/libycenv.h
+++ include/libycenv.h
@@ -1,6 +1,8 @@
 #ifndef LIBYCENV_INCLUDE_H
 #define LIBYCENV_INCLUDE_H
 
-#include <world/world.h>
+#include "types/types.h"
+#include "statuses/statuses.h"
+#include "methods/methods.h"
 
 #endif //LIBYCENV_INCLUDE_H
--- source/world.c
+++ source/world.c
@@ -3,24 +3,25 @@
 
 #include <stdlib.h>
 
-yc_env_world_status_t yc_env_world_init(yc_res_map_t *source, yc_env_world_t **destination) {
-    if (NULL == source) { return YC_ENV_WORLD_STATUS_MEM; }
+yc_env_world_status_t yc_env_world_init(yc_res_map_t *source, yc_env_world_t **result) {
+    if (NULL == source) { return YC_ENV_STATUS_INPUT; }
+    if (NULL == result) { return YC_ENV_STATUS_INPUT; }
 
-    yc_env_world_t *result = malloc(sizeof(yc_env_world_t));
-    if (NULL == result) { return YC_ENV_WORLD_STATUS_MEM; }
+    yc_env_world_t *world = malloc(sizeof(yc_env_world_t));
+    if (NULL == world) { return YC_ENV_STATUS_MEM; }
 
-    result->map = source;
-    *destination = result;
+    world->map = source;
+    *result = world;
 
-    return YC_ENV_WORLD_STATUS_OK;
+    return YC_ENV_STATUS_OK;
 }
 
 yc_env_world_status_t yc_env_world_tick(yc_env_world_t *world) {
-    if (NULL == world) { return YC_ENV_WORLD_STATUS_MEM; }
-    if (NULL == world->map) { return YC_ENV_WORLD_STATUS_MEM; }
+    if (NULL == world) { return YC_ENV_STATUS_MEM; }
+    if (NULL == world->map) { return YC_ENV_STATUS_MEM; }
 
     // TODO: Advance the world's state to next tick.
     world->map->ticks += 1;
 
-    return YC_ENV_WORLD_STATUS_OK;
+    return YC_ENV_STATUS_OK;
 }
