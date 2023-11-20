af4de19fb6216b0c81eaccac47425d63248b133e
Alexander Orlov <me@0xceed.com>
2023-11-20 03:18:32 +0300

    Drafted minimal API.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- include/methods/methods.h
+++ include/methods/methods.h
@@ -1,7 +1,9 @@
 #ifndef LIBYCENV_INCLUDE_METHODS_H
 #define LIBYCENV_INCLUDE_METHODS_H
 
-yc_env_world_status_t yc_env_world_init(yc_res_map_t *source, yc_env_world_t **result);
+yc_env_world_status_t yc_env_world_initialize(yc_env_world_t *result, yc_res_map_t *map);
+yc_env_world_status_t yc_env_world_invalidate(yc_env_world_t *world);
+
 yc_env_world_status_t yc_env_world_tick(yc_env_world_t *world);
 
 #endif //LIBYCENV_INCLUDE_METHODS_H
--- source/world.c
+++ source/world.c
@@ -1,17 +1,19 @@
 #include <libycenv.h>
 #include <libycres.h>
 
-#include <stdlib.h>
+yc_env_world_status_t yc_env_world_initialize(yc_env_world_t *world, yc_res_map_t *map) {
+    if (NULL == map) { return YC_ENV_STATUS_INPUT; }
+    if (NULL == world) { return YC_ENV_STATUS_INPUT; }
 
-yc_env_world_status_t yc_env_world_init(yc_res_map_t *source, yc_env_world_t **result) {
-    if (NULL == source) { return YC_ENV_STATUS_INPUT; }
-    if (NULL == result) { return YC_ENV_STATUS_INPUT; }
+    world->map = map;
 
-    yc_env_world_t *world = malloc(sizeof(yc_env_world_t));
-    if (NULL == world) { return YC_ENV_STATUS_MEM; }
+    return YC_ENV_STATUS_OK;
+}
 
-    world->map = source;
-    *result = world;
+yc_env_world_status_t yc_env_world_invalidate(yc_env_world_t *world) {
+    if (NULL == world) { return YC_ENV_STATUS_INPUT; }
+
+    yc_res_map_invalidate(world->map);
 
     return YC_ENV_STATUS_OK;
 }
